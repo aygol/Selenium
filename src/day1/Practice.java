af5e1619d7d310536f1de83702115e3ee2e0eba9
Agolcu <golcuayse@hotmail.com>
2020-02-29 19:42:03 -0500

    adding new line

--- src/day1/Practice.java
+++ src/day1/Practice.java
@@ -3,5 +3,7 @@ package day1;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Thank you Furkan everything ");
+
     }
 }
