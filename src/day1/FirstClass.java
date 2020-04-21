82258f0fbcb0ad5351b5602e592b85a1fe53cacd
Agolcu <golcuayse@hotmail.com>
2020-04-21 00:29:20 -0400

    change

--- src/day1/FirstClass.java
+++ src/day1/FirstClass.java
@@ -4,5 +4,7 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("Doing first Sprint meeting");
+        System.out.println("git learn merge");
+
     }
 }
