f88f9d741649203f200cecbdcb7946a5808e6e4c
Ayse Golcu <58408352+aygol@users.noreply.github.com>
2020-04-21 00:32:56 -0400

    Update FirstClass.java

--- src/day1/FirstClass.java
+++ src/day1/FirstClass.java
@@ -4,7 +4,9 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("Doing first Sprint meeting");
-        System.out.println("git learn merge");
+         System.out.println("Doing second Sprint meeting");
+         System.out.println("Doing third Sprint meeting");
+    
 
     }
 }
