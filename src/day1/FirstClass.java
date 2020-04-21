cbb8e8aeb31d1bd19c5dc847413ecae5582d18dd
Ayse Golcu <58408352+aygol@users.noreply.github.com>
2020-04-21 00:37:54 -0400

    Update FirstClass.java

--- src/day1/FirstClass.java
+++ src/day1/FirstClass.java
@@ -4,8 +4,7 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("Doing first Sprint meeting");
-         System.out.println("Doing second Sprint meeting");
-         System.out.println("Doing third Sprint meeting");
+     
     
 
     }
